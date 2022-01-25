05f59f9f0199d7708568fbfdb0397fa06c9d1c44
Shubham Prince <44049342+princekhg@users.noreply.github.com>
2022-01-25 22:16:15 +0530

    LINQ example to print frequency
    This is a simple C# program that is used to print the frequency of each character in the entered string.

--- Program.cs
+++ Program.cs
@@ -1,59 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System;   
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloWorld
+// Including Linq
+public class LINQ
 {
-    class Program
+    string str=" ";
+    // method to read the string 
+    public void read()
     {
-        int[] num;
-        // program to read value in the array
-        public void read()
+        Console.WriteLine("Input String  ---- ");
+        str = Console.ReadLine();   
+    }
+    // MEthod to Count and display the output of each string 
+    public void CountDisplay()
+    {
+        // Since upper or lower case doesn't change the character.
+        // I've converted the value to UpperCase for ease.
+        //Can be a choice but better to for user understanding
+        str = str.ToUpper();
+        //Creating LINQ query by grouping each character in string
+        //and then into a variable vari and then selecting vari as the final
+        //value to be added to countFreq variable.
+        var countFreq = from s in str
+                        group s by s into vari
+                        select vari;
+        //Since the Groupby provides a key we can use key to print values and aggregate 
+        //function Count can add all the values to show total count
+        Console.WriteLine("The Frquency of Each Character in the given String is --- ");
+        foreach (var c in countFreq)
         {
-            int Len;
-            Console.WriteLine("Enter length of array");
-            Len = Convert.ToInt32(Console.ReadLine());
-            num = new int[Len];
-            Console.WriteLine("Enter array values");
-            for (int i = 0; i < Len; i++)
-                num[i] = Convert.ToInt32(Console.ReadLine());
+            //White space character can be intercepted inside name
+            // So if will check and avoid that key and loop can resume to next iteration
+            // This is easy..
+            if (Char.IsWhiteSpace(c.Key))
+                continue;
+            Console.WriteLine("Character " + c.Key + " has frequency of - " + c.Count());
         }
-        // method to perform Buuble Sort
-        public void BubbleSort()
-        {
-            int temp;
-            for (int i = 0; i < num.Length; i++)
-            {
-                for (int j = 0; j < num.Length - i - 1; j++)
-                { // method to perform elemnts swapping
-                    if (num[j] > num[j + 1])
-                    {
-                        temp = num[j];
-                        num[j] = num[j + 1];
-                        num[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        // method to display the sorted array
-        public void display()
-        {
-            foreach (int n in num)
-            {
-                Console.Write(n + ",");
-            }
-            Console.Read();
-        }
-        // Final calling method to call the objects 
-        static void Main(string[] args)
-        {
-            Program pg = new Program();
-            pg.read();
-            pg.BubbleSort();
-            pg.display();
-        }
+    }
+    public static void Main(string[] args)
+    {
+        // Creating object of class
+        LINQ linq=new LINQ();
+        // calling read method 
+        linq.read();
+        //Calling CountDisplay()
+        linq.CountDisplay();
     }
 }
